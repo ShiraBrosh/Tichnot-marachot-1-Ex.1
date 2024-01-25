2927a913187a64f8c4016dac1368d212c3b64385
ShiraBrosh <155542654+ShiraBrosh@users.noreply.github.com>
2024-01-25 18:58:33 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,56 +1,51 @@
-#include <stdio.h>
 #include "NumClass.h"
 
-int main() {
+
+int main (){
+    int num1, num2, max,min;
+
+    scanf("%d%d",&num1,&num2);
     
-     //printf("isPrime(7): %d\n", isPrime(7));
-
-    int first_number;
-    int second_number;
-    int max;
-    int min;
-    int a,b;
-
-    scanf("%d%d",&a,&b);
-
-    if(first_number > second_number ){
-        max = first_number;
-        min = second_number;
-    }else{
-        max = second_number;
-        min = first_number;
+    if (num1>num2){
+        max=num1;
+        min=num2;
+    }
+    else{
+        max=num2;
+        min=num1;
     }
 
-    printf("The Armstrong numbers are:");
-    for (int i = a; i <= b; ++i)
-    {
-        if (isArmstrong(i))
-            printf(" %d", i);
+printf("The Armstrong numbers are:");
+for (int i=min; i<=max; i++){
+            if  (isArmstrong(i)){
+                printf(" %d",i);
+            }
+        }
+
+
+printf("\nThe Palindromes are:");
+ for (int i=min; i<=max; i++){
+        if  (isPalindrome(i)){
+             printf(" %d",i);
+        
+    }
+ }
+printf("\nThe Prime numbers are:");
+for (int i=min; i<=max; i++){
+        if  (isPrime(i)){
+             printf(" %d",i); 
+        }
     }
 
-    printf("\nThe Palindromes are:");
-    for (int i = a; i <= b; ++i)
-    {
-        if (isPalindrome(i))
-            printf(" %d", i);
+printf("\nThe Strong numbers are:"); 
+ for (int i=min; i<=max; i++){
+        if  (isStrong(i)){
+             printf(" %d",i);
+        }
     }
+printf("\n");
 
-    printf("\nThe Prime numbers are:");
-    for (int i = a; i <= b; ++i)
-    {
-        if (isPrime(i))
-            printf(" %d", i);
-    }
-    
-    printf("\nThe Strong numbers are:");
-    for (int i = a; i <= b; ++i)
-    {
-        if (isStrong(i))
-             printf(" %d", i);
-    }
+return 0;
 
-    printf("\n");
 
-    return 0;
-   }
-
+}
